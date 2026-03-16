0dd7711e4216ec26444c8827ef1eeb11762a4854
Swapnil Acharya <acharyaswapnil79@gmail.com>
2026-03-16 08:23:34 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8965658957-bbb9b",
+  "appId": "1:852674847436:web:cec3212f1b13a871729869",
+  "apiKey": "AIzaSyBHpffYEUe1Xx6pV6r9oGRRhT4OYKsgF5Y",
+  "authDomain": "studio-8965658957-bbb9b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "852674847436"
+};
